294308baf88061706d10937a806b0d429954e238
Martin Hostettler <textshell@uchuujin.de>
2017-07-15 17:53:00 +0200

    utf8: add check_valid_sequence

--- termpaint_utf8.h
+++ termpaint_utf8.h
@@ -3,6 +3,30 @@
 
 // internal header, not api or abi stable
 
+/*
+  x = any bit value
+  y = at least one needs to be set (to detect overlong encodings which are invalid)
+
+0x00000000 - 0x0000007F:
+    0xxxxxxx (7bit)
+
+0x00000080 - 0x000007FF:
+    110yyyyx 10xxxxxx (11bit)
+
+0x00000800 - 0x0000D7FF:
+0x0000E000 - 0x0000FFFD:
+    1110yyyy 10yxxxxx 10xxxxxx (16 bit)
+
+0x00010000 - 0x001FFFFF:
+    11110yyy 10yyxxxx 10xxxxxx 10xxxxxx (21bit)
+
+0x00200000 - 0x03FFFFFF:
+    111110yy 10yyyxxx 10xxxxxx 10xxxxxx 10xxxxxx (26bit)
+
+0x04000000 - 0x7FFFFFFF:
+    1111110y 10yyyyxx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx (31bit)
+*/
+
 static inline int termpaintp_utf8_len(unsigned char first_byte) {
     int size;
     if (0xfc == (0xfe & first_byte)) {
@@ -19,6 +43,64 @@ static inline int termpaintp_utf8_len(unsigned char first_byte) {
         size = 1;
     }
     return size;
+}
+
+static inline _Bool termpaintp_check_valid_sequence(const unsigned char *input, unsigned length) {
+#define CHECK_CONTINUATION_BYTE(index) if ((input[index] & 0xc0) != 0x80) return false;
+    if (length == 0) {
+        return 0;
+    }
+    if ((*input >= 0x80 && *input <= 0xc0) || *input == 0xfe || *input == 0xff) {
+        return 0;
+    }
+    unsigned len = termpaintp_utf8_len(*input);
+    if (len != length) {
+        return 0;
+    }
+    if (len == 1) {
+        return 1;
+    } else if (len == 2) {
+        CHECK_CONTINUATION_BYTE(1)
+        return input[0] & 0x1e;
+    } else if (len == 3) {
+        CHECK_CONTINUATION_BYTE(1)
+        CHECK_CONTINUATION_BYTE(2)
+        //unsigned codepoint = ((input[0] & 0x0f) << 12) | ((input[1] & 0x7f) << 6) | (input[2] & 0x7f);
+        //return (codepoint >= 0x800 && codepoint < 0xd800) || codepoint >= 0xe000;
+        // based on table 3-7 in unicode standard (9.0)
+        if (input[0] == 0xe0) {
+            // all variable bits in input[0] are zero
+            // most significant variable bit in input[0] must be set if this is not an overlong sequence
+            return input[1] >= 0xa0;
+        } else if (input[0] == 0xed) {
+            // decoded value begins with 1101
+            // if code continues with 1xxxxx (thus 1101 1xxxxx ...) it is surrogate
+            // thus not a valid unicode scalar value
+            return input[1] < 0xa0;
+        } else {
+            return 1;
+        }
+    } else if (len == 4) {
+        CHECK_CONTINUATION_BYTE(1)
+        CHECK_CONTINUATION_BYTE(2)
+        CHECK_CONTINUATION_BYTE(3)
+        return (input[0] & 0x7) + (input[1] & 0x30);
+    } else if (len == 5) {
+        CHECK_CONTINUATION_BYTE(1)
+        CHECK_CONTINUATION_BYTE(2)
+        CHECK_CONTINUATION_BYTE(3)
+        CHECK_CONTINUATION_BYTE(4)
+        return (input[0] & 0x3) + (input[1] & 0x38);
+    } else { // len == 6
+        CHECK_CONTINUATION_BYTE(1)
+        CHECK_CONTINUATION_BYTE(2)
+        CHECK_CONTINUATION_BYTE(3)
+        CHECK_CONTINUATION_BYTE(4)
+        CHECK_CONTINUATION_BYTE(5)
+        return (input[0] & 0x1) + (input[1] & 0x3c);
+    }
+    return 0; // not reached
+#undef CHECK_CONTINUATION_BYTE
 }
 
 // return count of bytes written
