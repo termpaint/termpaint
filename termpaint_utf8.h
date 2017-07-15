a66f0e25c6de1b861e43b3feca481921b56abd78
Martin Hostettler <textshell@uchuujin.de>
2017-07-15 17:50:46 +0200

    utf8: fix encode_to_utf8

--- termpaint_utf8.h
+++ termpaint_utf8.h
@@ -25,8 +25,8 @@ static inline int termpaintp_utf8_len(unsigned char first_byte) {
 // buffer needs to be 6 bytes long
 // does not reject UTF-16 surrogates codepoints
 static inline int termpaintp_encode_to_utf8(int codepoint, unsigned char *buf) {
-#define STORE_AND_SHIFT(index)              \
-    buf[index] = codepoint & 0b00111111;    \
+#define STORE_AND_SHIFT(index)                     \
+    buf[index] = (codepoint & 0b00111111) | 0x80;    \
     codepoint = codepoint >> 6;
 
     if (codepoint > 0x7FFFFFFF) {
@@ -38,29 +38,29 @@ static inline int termpaintp_encode_to_utf8(int codepoint, unsigned char *buf) {
         STORE_AND_SHIFT(3)
         STORE_AND_SHIFT(2)
         STORE_AND_SHIFT(1)
-        buf[0] = 0b1111100 | codepoint;
+        buf[0] = 0b11111100 | codepoint;
         return 6;
     } else if (codepoint >= 0x00200000) {
         STORE_AND_SHIFT(4)
         STORE_AND_SHIFT(3)
         STORE_AND_SHIFT(2)
         STORE_AND_SHIFT(1)
-        buf[0] = 0b1111000 | codepoint;
+        buf[0] = 0b11111000 | codepoint;
         return 5;
     } else if (codepoint >= 0x00010000) {
         STORE_AND_SHIFT(3)
         STORE_AND_SHIFT(2)
         STORE_AND_SHIFT(1)
-        buf[0] = 0b1110000 | codepoint;
+        buf[0] = 0b11110000 | codepoint;
         return 4;
     } else if (codepoint >= 0x00000800) {
         STORE_AND_SHIFT(2)
         STORE_AND_SHIFT(1)
-        buf[0] = 0b1111000 | codepoint;
+        buf[0] = 0b11100000 | codepoint;
         return 3;
     } else if (codepoint >= 0x00000080) {
         STORE_AND_SHIFT(1)
-        buf[0] = 0b1111100 | codepoint;
+        buf[0] = 0b11000000 | codepoint;
         return 2;
     } else {
         buf[0] = codepoint;
