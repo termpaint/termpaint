694ae7335cecbb9fb7d63e434ccc948a20c4d014
Martin Hostettler <textshell@uchuujin.de>
2017-10-29 19:23:42 +0100

    termpaint.h: Structure

--- termpaint.h
+++ termpaint.h
@@ -25,11 +25,12 @@ typedef struct termpaint_integration_ {
 
 termpaint_surface *termpaint_surface_new(termpaint_integration *integration);
 void termpaint_surface_resize(termpaint_surface *surface, int width, int height);
-void termpaint_surface_flush(termpaint_surface *surface);
 void termpaint_surface_write_with_colors(termpaint_surface *surface, int x, int y, const char *string, int fg, int bg);
 void termpaint_surface_clear(termpaint_surface *surface, int bg);
-void termpaint_surface_reset_attributes(termpaint_surface *surface);
 
+// May only be called on terminal surfaces
+void termpaint_surface_reset_attributes(termpaint_surface *surface);
+void termpaint_surface_flush(termpaint_surface *surface);
 _Bool termpaint_auto_detect(termpaint_surface *surface);
 
 #ifdef __cplusplus
